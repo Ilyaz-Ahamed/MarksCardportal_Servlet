be734ef86e2ae2ffb48c8d5591f90888ab54d4c2
KarthikSR98 <ilyazahmed2428@gmail.com>
2023-10-01 22:19:42 +0530

    push code

---
+++ src/main/java/Markscard/Marksftechid.java
@@ -0,0 +1,37 @@
+package Markscard;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Markscarddao.Marksdao;
+import Markscarddto.Marksdto;
+@WebServlet("/student")
+public class Marksftechid extends HttpServlet {
+@Override
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	//super.doGet(req, resp);
+	
+	String usn=req.getParameter("USN");
+	long usn1=Long.parseLong(usn);
+	
+	Marksdao dao=new Marksdao();
+	Marksdto dto=dao.fetchid(usn1);
+	if(dto!=null) {
+		req.setAttribute("Ilyaz", dto);
+		RequestDispatcher rd=req.getRequestDispatcher("fetchid.jsp");
+		rd.forward(req, resp);
+	}
+	else {
+		resp.sendRedirect("wrongusn.jsp");
+	}
+	
+	//resp.getWriter().print(dto);
+}
+}
